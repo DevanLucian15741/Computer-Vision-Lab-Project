3e70d6539f7b1fb9f900c1f8ab39bc7268593046
DevanLucian15741 <132327217+DevanLucian15741@users.noreply.github.com>
2023-12-18 08:44:02 +0700

    Update Face_Reconition.py

--- Face_Reconition.py
+++ Face_Reconition.py
@@ -4,28 +4,31 @@ import os
 
 def Face_Recognition_Testing():
     People = []
-
-    for i in os.listdir(r'C:\Users\asus\Downloads\Computer Vision Project\Dataset'):
+    for i in os.listdir('Dataset'):
         People.append(i)
 
     haar_cascade = cv2.CascadeClassifier('HaarFace_Classifier.xml')
-
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    
+    try:
+        face_recognizer.read("Model.yml")    
+    except cv2.error as e:
+        return
+    
     face_recognizer.read("Model.yml")
-
     Directory = input("Input Absolute Path to Predict Image: ")
 
     img = cv2.imread(Directory)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Person',gray)
-    faces_rect =  haar_cascade.detectMultiScale(gray, 1.1, 4)
+    faces_rect =  haar_cascade.detectMultiScale(gray, 1.1, 30)
 
     for (x,y,w,h) in faces_rect:
         faces = gray[y:y+h,x:x+w]
         faces = cv2.bilateralFilter(faces, 5, 200, 200)
         faces = cv2.GaussianBlur(faces, (11,11), 0, None)
-        faces = cv2.resize(faces, (50,50), interpolation = cv2.INTER_AREA)
+        faces = cv2.cornerHarris(faces, 2, 9, 0.04)
+        faces = cv2.resize(faces, (69,69), interpolation = cv2.INTER_AREA)
         label, confidence = face_recognizer.predict(faces)
 
         if (confidence < 100):
